2a245555c0c19f0cc00fa35ec05bc3d0d6c611f2
oxidationreductionreaction <oxidationreductionreaction@gmail.com>
2014-12-04 13:36:15 +0100

    fix bug

--- toolkit/biprecmmd.c
+++ toolkit/biprecmmd.c
@@ -405,7 +405,7 @@ static double *create_psimM(struct LineFile *simf, int maxId) {
 	double *psimM = smalloc((maxId + 1)*(maxId + 1) * sizeof(double));
 	int i;
 	for (i = 0; i <(maxId + 1) * (maxId + 1); ++i) {
-		psimM = 0;	
+		psimM[i] = 0;	
 	}
 	for (i = 0; i < simf->linesNum; ++i) {
 		psimM[simf->i1[i] * (maxId + 1) + simf->i2[i]] = simf->d1[i];
@@ -488,6 +488,7 @@ static void do_work_divide_score(struct OptionArgs *oa) {
 		free_Bip(tr1); free_Bip(tr2);
 		free_Bip(te1); free_Bip(te2);
 		free_iidNet(trsim);
+		free(psimM);
 	}
 
 	int loopNum = oa->loopNum;
